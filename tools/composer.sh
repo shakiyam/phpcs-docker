bdb15fc8f72f921427a56ff3d7f7117226d15fe8
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-12-03 08:29:45 +0000

    Use constant for Composer Docker image version

--- tools/composer.sh
+++ tools/composer.sh
@@ -6,6 +6,8 @@ readonly SCRIPT_DIR
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR"/colored_echo.sh
 
+readonly COMPOSER_IMAGE="docker.io/composer:2.9"
+
 [[ -f composer.lock ]] || echo "{}" >composer.lock
 if [[ -d "$PWD"/vendor ]]; then
   if command -v docker &>/dev/null; then
@@ -16,7 +18,7 @@ if [[ -d "$PWD"/vendor ]]; then
       -v "$PWD"/composer.json:/app/composer.json:ro \
       -v "$PWD"/composer.lock:/app/composer.lock \
       -v "$PWD"/vendor:/app/vendor \
-      docker.io/composer:2.9 composer "$@"
+      "$COMPOSER_IMAGE" composer "$@"
   elif command -v podman &>/dev/null; then
     podman container run \
       --name "composer_$(uuidgen | head -c8)" \
@@ -25,7 +27,7 @@ if [[ -d "$PWD"/vendor ]]; then
       -v "$PWD"/composer.json:/app/composer.json:ro \
       -v "$PWD"/composer.lock:/app/composer.lock \
       -v "$PWD"/vendor:/app/vendor \
-      docker.io/composer:2.9 composer "$@"
+      "$COMPOSER_IMAGE" composer "$@"
   else
     echo_error 'Neither docker nor podman is installed.'
     exit 1
@@ -39,7 +41,7 @@ else
       -v "$PWD"/composer.json:/tmp/composer.json:ro \
       -v "$PWD"/composer.lock:/tmp/composer.lock \
       -w /tmp \
-      docker.io/composer:2.9 composer "$@"
+      "$COMPOSER_IMAGE" composer "$@"
   elif command -v podman &>/dev/null; then
     podman container run \
       --name "composer_$(uuidgen | head -c8)" \
@@ -48,7 +50,7 @@ else
       -v "$PWD"/composer.json:/tmp/composer.json:ro \
       -v "$PWD"/composer.lock:/tmp/composer.lock \
       -w /tmp \
-      docker.io/composer:2.9 composer "$@"
+      "$COMPOSER_IMAGE" composer "$@"
   else
     echo_error 'Neither docker nor podman is installed.'
     exit 1
